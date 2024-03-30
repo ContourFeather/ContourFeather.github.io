5f4fdabde71f870d5ca999f3e4593e3ec4722bff
ContourFeather <156165136+ContourFeather@users.noreply.github.com>
2024-03-30 09:42:32 -0700

    Add files via upload

---
+++ flightofabluejay/getimages.js
@@ -0,0 +1,9 @@
+function getImages(){
+    images = {
+        jaySprite: [loadImage("./assets/sprites/bluejayspriteleft.png"), loadImage("./assets/sprites/bluejaysprite.png")],
+        jayFlight1:[loadImage("./assets/sprites/bluejayflight1left.png"), loadImage("./assets/sprites/bluejayflight1.png")],
+        jayFlight2:[loadImage("./assets/sprites/bluejayflight2left.png"), loadImage("./assets/sprites/bluejayflight2.png")],
+        nest: loadImage("./assets/sprites/nest.png"),
+        fruitTree: [loadImage("./assets/sprites/fruittree/emptytree.png"), loadImage("./assets/sprites/fruittree/levelone.png"), loadImage("./assets/sprites/fruittree/leveltwo.png"), loadImage("./assets/sprites/fruittree/levelthree.png"), loadImage("./assets/sprites/fruittree/levelfour.png")]
+    };
+}
