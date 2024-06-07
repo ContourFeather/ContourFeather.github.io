7419fc7197236e95a9924df11febbe6683ec7077
ContourFeather <156165136+ContourFeather@users.noreply.github.com>
2024-06-06 22:57:16 -0700

    Update getimages.js

--- flightofabluejay/getimages.js
+++ flightofabluejay/getimages.js
@@ -1,13 +1,14 @@
 function getImages(){
+    console.log("Load Images");
     images = {
-        jaySprite: [loadImage("./assets/sprites/bluejayspriteleft.png"), loadImage("./assets/sprites/bluejaysprite.png")],
-        jayFlight1:[loadImage("./assets/sprites/bluejayflight1left.png"), loadImage("./assets/sprites/bluejayflight1.png")],
-        jayFlight2:[loadImage("./assets/sprites/bluejayflight2left.png"), loadImage("./assets/sprites/bluejayflight2.png")],
+        jaySprite: [loadImage("./assets/Sprites/bluejayspriteleft.png"), loadImage("./assets/Sprites/bluejaysprite.png")],
+        jayFlight1:[loadImage("./assets/Sprites/bluejayflight1left.png"), loadImage("./assets/Sprites/bluejayflight1.png")],
+        jayFlight2:[loadImage("./assets/Sprites/bluejayflight2left.png"), loadImage("./assets/Sprites/bluejayflight2.png")],
         
-        hawkFlight1:[loadImage("./assets/sprites/hawkflight1left.png"), loadImage("./assets/sprites/hawkflight1.png")],
-        hawkFlight2:[loadImage("./assets/sprites/hawkflight2left.png"), loadImage("./assets/sprites/hawkflight2.png")],
+        hawkFlight1:[loadImage("./assets/Sprites/hawkflight1left.png"), loadImage("./assets/Sprites/hawkflight1.png")],
+        hawkFlight2:[loadImage("./assets/Sprites/hawkflight2left.png"), loadImage("./assets/Sprites/hawkflight2.png")],
         
-        nest: loadImage("./assets/sprites/nest.png"),
-        fruitTree: [loadImage("./assets/sprites/fruittree/emptytree.png"), loadImage("./assets/sprites/fruittree/levelone.png"), loadImage("./assets/sprites/fruittree/leveltwo.png"), loadImage("./assets/sprites/fruittree/levelthree.png"), loadImage("./assets/sprites/fruittree/levelfour.png")]
+        nest: loadImage("./assets/Sprites/nest.png"),
+        fruitTree: [loadImage("./assets/Sprites/fruittree/emptytree.png"), loadImage("./assets/Sprites/fruittree/levelone.png"), loadImage("./assets/Sprites/fruittree/leveltwo.png"), loadImage("./assets/Sprites/fruittree/levelthree.png"), loadImage("./assets/Sprites/fruittree/levelfour.png")]
     };
 }
